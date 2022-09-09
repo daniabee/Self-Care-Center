1876a40f69503ffbd1d35d36906c1d4bb67b90d3
Dani Bagley <bagley.a.danielle@gmail.com>
2022-09-09 11:04:42 -0600

    Add clear button feature and error handling

--- main.js
+++ main.js
@@ -3,22 +3,35 @@ var mantraChecked = document.getElementById('mantra');
 var recieveMessageButton = document.querySelector('#recieve-message-button');
 var meditationImage = document.querySelector('svg');
 var meditationContainer = document.querySelector('#meditate-container');
+var clearMessageButton = document.querySelector('#clear-button');
+var message = ''
+var paragraph = document.querySelector('#paragraph');
 
 recieveMessageButton.addEventListener('click',giveMessage);
+clearMessageButton.addEventListener('click', clearMessage);
+
 
 
 function giveMessage() {
   if (mantra.checked) {
-    var message = mantras[getRandomIndex(mantras)];
+    message = mantras[getRandomIndex(mantras)];
     meditationImage.classList.add('hidden');
-    meditationContainer.innerHTML = `
+    paragraph.innerHTML = `
     <p>${message}</p>`
   }
   if (affirmation.checked) {
-    var message = affirmations[getRandomIndex(affirmations)];
+    message = affirmations[getRandomIndex(affirmations)];
     meditationImage.classList.add('hidden');
-    meditationContainer.innerHTML = `
+    paragraph.innerHTML = `
     <p>${message}</p>`
+  }
+}
+
+function clearMessage() {
+  if (meditationImage.classList.contains('hidden')) {
+    meditationImage.classList.remove('hidden');
+    meditationImage.classList.add('visible');
+    document.querySelector('p').innerHTML = '';
   }
 }
 
